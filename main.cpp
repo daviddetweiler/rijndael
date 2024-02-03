dba8bd1434a89ed4dbf692f7192250c54e5697e2
David Detweiler <daviddetweiler1@gmail.com>
2024-02-03 01:34:49 -0800

    Refactor

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 #include <cstdint>
 #include <cstring>
 #include <format>
+#include <functional>
 #include <iostream>
 #include <memory>
 #include <string_view>
-#include <tuple>
 #include <utility>
 #include <vector>
 
@@ -33,18 +33,33 @@ namespace rijndael {
 			return gsl::narrow_cast<std::uint32_t>(a | b << 8 | c << 16 | d << 24);
 		};
 
-		class constant_table {
-		public:
+		template <unsigned int i>
+		static constexpr std::uint8_t rotate(std::uint8_t b)
+		{
+			return (b << i) | (b >> (7 & (0u - i)));
+		}
+
+		static constexpr std::uint8_t times_x(std::uint8_t n)
+		{
+			const auto t = gsl::narrow_cast<std::uint8_t>(n << 1);
+			return (n & 0x80) ? t ^ 0x1b : t;
+		}
+
+		static constexpr std::uint8_t multiply(std::uint8_t a, std::uint8_t b)
+		{
+			std::uint8_t c {};
+			for (; a; a >>= 1, b = times_x(b))
+				c ^= (a & 0x01) ? b : 0x0u;
+
+			return c;
+		}
+
+		struct constant_table {
 			std::array<std::uint8_t, (14 + 1) * 8 / 4> round_constants;
 			u_op sbox;
 			u_op inverse_sbox;
 			t_tables round;
 			t_tables inverse_round;
-
-			static auto make() { return std::make_unique<const constant_table>(); }
-
-		private:
-			friend std::unique_ptr<const constant_table> std::make_unique<const constant_table>();
 
 			[[gsl::suppress(bounds)]] [[gsl::suppress(f .6)]] constant_table() :
 				round_constants {},
@@ -103,27 +118,6 @@ namespace rijndael {
 				times_table.reset();
 				inverse_table.reset();
 			}
-
-			template <unsigned int i>
-			static constexpr std::uint8_t rotate(std::uint8_t b)
-			{
-				return (b << i) | (b >> (7 & (0u - i)));
-			}
-
-			static constexpr std::uint8_t times_x(std::uint8_t n)
-			{
-				const auto t = gsl::narrow_cast<std::uint8_t>(n << 1);
-				return (n & 0x80) ? t ^ 0x1b : t;
-			}
-
-			static constexpr std::uint8_t multiply(std::uint8_t a, std::uint8_t b)
-			{
-				std::uint8_t c {};
-				for (; a; a >>= 1, b = times_x(b))
-					c ^= (a & 0x01) ? b : 0x0u;
-
-				return c;
-			}
 		};
 
 		static constexpr std::array shifts {
@@ -135,8 +129,7 @@ namespace rijndael {
 		};
 
 		template <unsigned int nk, unsigned int nb>
-		class [[gsl::suppress(bounds)]] rijndael
-		{
+		class [[gsl::suppress(bounds)]] transcoder {
 			static_assert(nk >= 4 && nb >= 4 && nk <= 8 && nb <= 8, "not a valid Rijndael cipher");
 
 		public:
@@ -147,7 +140,7 @@ namespace rijndael {
 			using key_view = std::span<const std::uint8_t, key_size>;
 			using block_view = std::span<std::uint8_t, block_size>;
 
-			[[gsl::suppress(gsl.view)]] rijndael(const constant_table& c, key_view k) noexcept : rijndael {}
+			[[gsl::suppress(gsl.view)]] transcoder(const constant_table& c, key_view k) noexcept : transcoder {}
 			{
 				apply_key(c, k);
 			}
@@ -175,11 +168,10 @@ namespace rijndael {
 			round_keys keys {};
 			round_keys inverse_keys {};
 
-			rijndael() = default;
+			transcoder() = default;
 
-			[[gsl::suppress(bounds)]] [[gsl::suppress(gsl.view)]] static void inverse_mix_column(
-				const constant_table& table,
-				column c) noexcept
+			[[gsl::suppress(bounds)]] [[gsl::suppress(gsl.view)]] static void
+			inverse_mix_column(const constant_table& table, column c) noexcept
 			{
 				const auto& [t0, t1, t2, t3] = table.inverse_round;
 				const auto& sbox = table.sbox;
@@ -203,8 +195,8 @@ namespace rijndael {
 				return pack(x, y, z, w);
 			}
 
-			[[gsl::suppress(bounds)]] [[gsl::suppress(gsl.view)]] void apply_key_schedule(
-				const constant_table& table) noexcept
+			[[gsl::suppress(bounds)]] [[gsl::suppress(gsl.view)]] void
+			apply_key_schedule(const constant_table& table) noexcept
 			{
 				const std::span key_view {keys};
 				for (auto j = nk; j < n_key_words; ++j) {
@@ -229,9 +221,8 @@ namespace rijndael {
 			}
 
 			template <bool inverted>
-			[[gsl::suppress(bounds)]] [[gsl::suppress(gsl.view)]] void apply_rounds(
-				const constant_table& table,
-				block_view state) const noexcept
+			[[gsl::suppress(bounds)]] [[gsl::suppress(gsl.view)]] void
+			apply_rounds(const constant_table& table, block_view state) const noexcept
 			{
 				const std::span key_view {inverted ? inverse_keys : keys};
 				const auto offset = [](unsigned int r) { return (inverted ? n_rounds - r : r) * block_size; };
@@ -264,10 +255,8 @@ namespace rijndael {
 			}
 
 			template <bool inverted>
-			[[gsl::suppress(bounds)]] [[gsl::suppress(gsl.view)]] static constexpr auto get_byte(
-				block_view state,
-				std::uint64_t r,
-				std::uint64_t c)
+			[[gsl::suppress(bounds)]] [[gsl::suppress(gsl.view)]] static constexpr auto
+			get_byte(block_view state, std::uint64_t r, std::uint64_t c)
 			{
 				static constexpr std::span row_shifts = shifts[nb - 4];
 				const auto shift = row_shifts[r];
@@ -310,9 +299,9 @@ namespace rijndael {
 			}
 		};
 
-		using aes128 = rijndael<4, 4>;
-		using aes192 = rijndael<6, 4>;
-		using aes256 = rijndael<8, 4>;
+		using aes128 = transcoder<4, 4>;
+		using aes192 = transcoder<6, 4>;
+		using aes256 = transcoder<8, 4>;
 
 		[[gsl::suppress(bounds .1)]] void print_blob(gsl::span<std::uint8_t> blob)
 		{
@@ -324,71 +313,87 @@ namespace rijndael {
 
 		enum class mode { vectors, multiply };
 
-		template <typename cipher_type>
-		[[gsl::suppress(gsl.view)]] [[gsl::suppress(bounds .1)]] void test(const constant_table& constants, mode m)
+		constexpr auto reps = 1ull << 24;
+
+		template <typename callable_type>
+		auto time(callable_type task) noexcept
 		{
-			std::array<std::uint8_t, cipher_type::key_size> key {};
+			using clock = std::chrono::high_resolution_clock;
+			const auto start = clock::now();
+			const auto start_c = __rdtsc();
+
+			for (auto i = 0u; i < reps; ++i)
+				task();
+
+			const auto stop_c = __rdtsc();
+			const auto stop = clock::now();
+			const auto elapsed_time = std::chrono::duration_cast<std::chrono::duration<double>>(stop - start);
+			const auto elapsed_cycles = static_cast<double>(stop_c - start_c);
+
+			return std::make_pair(elapsed_time, elapsed_cycles);
+		};
+
+		template <typename cipher_type>
+		[[gsl::suppress(gsl.view)]] void print_vectors(const constant_table& constants)
+		{
+			const std::array<std::uint8_t, cipher_type::key_size> key {};
+			const cipher_type cipher {constants, key};
+			std::array<std::uint8_t, cipher_type::block_size> block {};
+
+			cipher.encrypt(constants, block);
+			print_blob(block);
+			cipher.encrypt(constants, block);
+			print_blob(block);
+			cipher.decrypt(constants, block);
+			cipher.decrypt(constants, block);
+
+			for (const auto& b : block) {
+				if (b)
+					std::cout << "bad decrypt\n";
+			}
+
+			std::cout << '\n';
+		}
+
+		template <typename cipher_type>
+		[[gsl::suppress(gsl.view)]] void do_benchmark(const constant_table& constants)
+		{
+			static constexpr auto format_string
+				= "b{}-k{}: {:.1f} K/s ({:01.02f}), {:.1f} MiB/s E ({:01.02f}), {:.1f} MiB/s D ({:01.02f})\n";
+
+			const std::array<std::uint8_t, cipher_type::key_size> key {};
 			std::array<std::uint8_t, cipher_type::block_size> block {};
 			cipher_type cipher {constants, key};
-			if (m == mode::vectors) {
-				cipher.encrypt(constants, block);
-				print_blob(block);
-				cipher.encrypt(constants, block);
-				print_blob(block);
-				cipher.decrypt(constants, block);
-				cipher.decrypt(constants, block);
-				for (const auto& b : block) {
-					if (b)
-						std::cout << "bad decrypt\n";
-				}
 
-				std::cout << '\n';
-			}
-			else if (m == mode::multiply) {
-				const auto time = [](auto&& task) {
-					using clock = std::chrono::high_resolution_clock;
-					const auto start = clock::now();
-					const auto start_c = __rdtsc();
-					task();
-					const auto stop_c = __rdtsc();
-					const auto stop = clock::now();
-					return std::make_tuple(
-						std::chrono::duration_cast<std::chrono::duration<double>>(stop - start),
-						static_cast<double>(stop_c - start_c));
-				};
+			const auto [apply_key, rk_c] = time(std::bind(&cipher_type::apply_key, cipher, constants, key));
+			const auto [encrypt, e_c] = time(std::bind(&cipher_type::encrypt, cipher, constants, block));
+			const auto [decrypt, d_c] = time(std::bind(&cipher_type::decrypt, cipher, constants, block));
 
-				static constexpr auto reps = 1ull << 24;
-				const auto [apply_key, rk_c] = time([&] {
-					for (auto i = 0u; i < reps; ++i)
-						cipher.apply_key(constants, key);
-				});
+			const auto rkps = reps / apply_key.count();
+			const auto embps = reps * block.size() / (encrypt.count() * 1024 * 1024);
+			const auto dmbps = reps * block.size() / (decrypt.count() * 1024 * 1024);
+			static constexpr auto div = reps * cipher.block_size;
+			static constexpr auto div_k = reps * cipher.key_size;
 
-				const auto [encrypt, e_c] = time([&] {
-					for (auto i = 0u; i < reps; ++i)
-						cipher.encrypt(constants, block);
-				});
+			std::cout << std::format(
+				format_string,
+				block.size() * 8,
+				key.size() * 8,
+				rkps,
+				rk_c / div_k,
+				embps,
+				e_c / div,
+				dmbps,
+				d_c / div);
+		}
 
-				const auto [decrypt, d_c] = time([&] {
-					for (auto i = 0u; i < reps; ++i)
-						cipher.decrypt(constants, block);
-				});
-
-				const auto rkps = reps / apply_key.count();
-				const auto embps = reps * block.size() / (encrypt.count() * 1024 * 1024);
-				const auto dmbps = reps * block.size() / (decrypt.count() * 1024 * 1024);
-				static constexpr auto div = reps * cipher.block_size;
-				static constexpr auto div_k = reps * cipher.key_size;
-				std::cout << std::format(
-					"b{}-k{}: {:.1f} K/s ({:01.02f}), {:.1f} MiB/s E ({:01.02f}), {:.1f} MiB/s D ({:01.02f})\n",
-					block.size() * 8,
-					key.size() * 8,
-					rkps,
-					rk_c / div_k,
-					embps,
-					e_c / div,
-					dmbps,
-					d_c / div);
-			}
+		template <typename cipher_type>
+		void test(const constant_table& constants, mode m)
+		{
+			if (m == mode::vectors)
+				print_vectors<cipher_type>(constants);
+			else if (m == mode::multiply)
+				do_benchmark<cipher_type>(constants);
 		}
 
 		using blocks = std::make_integer_sequence<unsigned int, 5>;
@@ -396,7 +401,7 @@ namespace rijndael {
 		template <unsigned int k, unsigned int b, unsigned int... s>
 		void iter_blocks(const constant_table& table, mode m)
 		{
-			test<rijndael<k + 4, b + 4>>(table, m);
+			test<transcoder<k + 4, b + 4>>(table, m);
 			if constexpr (sizeof...(s) != 0)
 				iter_blocks<k, s...>(table, m);
 		}
@@ -437,17 +442,20 @@ namespace rijndael {
 
 int main(int argc, char** argv)
 {
+	using namespace rijndael;
+
 	const gsl::span arguments {argv, gsl::narrow_cast<std::size_t>(argc)};
 	if (argc != 2)
 		return 1;
 
 	const std::string_view mode {arguments[1]};
+	constexpr auto make = [] { return std::make_unique<constant_table>(); };
 	if (mode == "vectors")
-		rijndael::run_benchmarks(*rijndael::constant_table::make(), rijndael::mode::vectors);
+		run_benchmarks(*make(), mode::vectors);
 	else if (mode == "times")
-		rijndael::run_benchmarks(*rijndael::constant_table::make(), rijndael::mode::multiply);
+		run_benchmarks(*make(), mode::multiply);
 	else if (mode == "aes")
-		rijndael::benchmark_aes(*rijndael::constant_table::make());
+		benchmark_aes(*make());
 	else
 		return 1;
 }
